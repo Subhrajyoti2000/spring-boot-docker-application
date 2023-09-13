f04aad4d9d82a74d13c8a2837ac07cb56687d1f5
Subhrajyoti Sahoo <subhrjyotisahoo2000@gmail.com>
2023-09-13 19:36:01 +0530

    dockerized spring boot application

---
+++ src/main/java/com/org/springbootdocker/SpringBootDockerApplication.java
@@ -0,0 +1,21 @@
+package com.org.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+	
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Hello, Docker!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
